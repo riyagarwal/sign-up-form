5ebaecd71464e493321ce168e6757ae6fdb2d1be
Riya Agarwal <53482709+riyagarwal@users.noreply.github.com>
2023-01-08 13:16:51 +0530

    4th commit Validation complete

--- src/signup/Signup.js
+++ src/signup/Signup.js
@@ -4,24 +4,54 @@ import "Signup.css";
 function Signup() {
   const [data, setData] = useState({});
   const [dataArr, setDataArr] = useState([]);
+  const [error, setError] = useState(false);
 
   const verify = () => {
-    if(!dataArr.fullName || !dataArr.email || !dataArr.password || !dataArr.cnfPassword) {
-      // error
-      return
-    }
 
-    if(!dataArr.email.contains('@') || !dataArr.email.contains('.') || !dataArr.email.length >= 6 ) {
-      // error
-      return
+    // Empty fields
+    if (
+      !dataArr.fullName ||
+      !dataArr.email ||
+      !dataArr.password ||
+      !dataArr.cnfPassword
+    ) {
+      document.getElementById("error").textContent =
+        "Error: All the fields are mandatory";
+      setError(true);
+      return;
+    } 
+    
+    // Email verification
+    else if (
+      !dataArr.email.contains("@") ||
+      !dataArr.email.contains(".") ||
+      !dataArr.email.length >= 6
+    ) {
+      document.getElementById("error").textContent = "Enter correct email";
+      setError(true);
+      return;
     }
 
     // Password Validation
-    if(dataArr.password !== dataArr.cnfPassword) {
-      // error
-      return
+    else if (dataArr.password.length < 8) {
+      document.getElementById("error").textContent =
+        "Password should be atleast 8 characters long";
+      setError = true;
+      return;
     }
-  }
+
+    // Password match
+    else if (dataArr.password !== dataArr.cnfPassword) {
+      document.getElementById("error").textContent =
+        "Password and Confirm Password do not match";
+      setError(true);
+      return;
+    } 
+    
+    else {
+      document.getElementById("error").textContent = "Successfully signed up";
+    }
+  };
 
   return (
     <div>
@@ -30,33 +60,35 @@ function Signup() {
         <input
           placeholder="Full Name"
           onChange={(event) => {
-            setData({ 'fullName': event.target.value });
+            setData({ fullName: event.target.value });
             setDataArr([...dataArr, data]);
           }}
         />
         <input
           placeholder="Email"
           onChange={(event) => {
-            setData({ 'email': event.target.value });
+            setData({ email: event.target.value });
             setDataArr([...dataArr, data]);
           }}
         />
         <input
           placeholder="Password"
           onChange={(event) => {
-            setData({ 'password': event.target.value });
+            setData({ password: event.target.value });
             setDataArr([...dataArr, data]);
           }}
         />
         <input
           placeholder="Confirm Password"
           onChange={(event) => {
-            setData({ 'cnfPassword': event.target.value });
+            setData({ cnfPassword: event.target.value });
             setDataArr([...dataArr, data]);
           }}
         />
         <button onClick={verify}> Sign Up </button>
       </div>
+
+      <p id="error"> </p>
     </div>
   );
 }
